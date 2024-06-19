59a13cc68e5370c5013e050a2b89f346643617ab
Enzo Guilherme <enzoguihartmann@gmail.com>
2024-06-19 08:54:27 -0300

    fear:adicionado console.log para digitar os numeros

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
 function mdc(a, b) {
     if (b === 0) {
         return a;
@@ -5,13 +13,25 @@ function mdc(a, b) {
         return mdc(b, a % b);
     }
 }
+
+
 function mmc(a, b) {
     return Math.abs(a * b) / mdc(a, b);
 }
-let num1 = 12;
-let num2 = 18;
-let resultado = mmc(num1, num2);
 
-console.log(`MMC de ${num1} e ${num2} é: ${resultado}`);
 
+rl.question('Digite o primeiro número: ', (num1) => {
+    rl.question('Digite o segundo número: ', (num2) => {
+        
+        num1 = parseInt(num1);
+        num2 = parseInt(num2);
+
+        
+        let resultado = mmc(num1, num2);
+        console.log(`Você escolheu os números ${num1} e ${num2}.`);
+        console.log(`MMC de ${num1} e ${num2} é: ${resultado}`);
+
+        rl.close();
+    });
+});
 
